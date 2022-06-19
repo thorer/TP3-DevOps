ffbd674333e0365b0099c1ea007903dc01ab7c89
Thorerc <57194672+thorer@users.noreply.github.com>
2022-06-19 18:01:34 +0200

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,18 +1,30 @@
+import os
 import requests
 import json
-from flask import Flask
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from marshmallow import Schema, fields
+
+class WeatherQuerySchema(Schema):
+    key1 = fields.Str(required=True)
+    key2 = fields.Str(required=True)
 
 app = Flask(__name__)
+api = Api(app)
+schema = WeatherQuerySchema()
 
-@app.route('/', methods=['GET'])
-def main():
-    api_key = "240aa650f4db4e154a07d0459c30a347"
-    lat = "39.571625"
-    lon = "2.650544"
-    url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (lat, lon, api_key)
-    response = requests.get(url)
-    data = json.loads(response.text)
-    print(data)
+api_key = os.environ['API_KEY']
+
+class Weather(Resource):
+    def get(self):
+        errors = schema.validate(request.args)
+        url = "https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=%s" % (request.args['lat'], request.args['lon'], api_key)
+        response = requests.get(url)
+        data = json.loads(response.text)
+        return data
+
+api.add_resource(Weather, '/')
 
 
-app.run(port=8080)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",debug=True,port=80)
